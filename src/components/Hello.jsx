c81930e09f2cd3e3b919189e9331d257bef0768c
Alex-Reyne <creativereyne@gmail.com>
2022-01-31 22:37:46 -0600

    create hello component

---
+++ src/components/Hello.jsx
@@ -0,0 +1,12 @@
+export default function Hello() {
+	return (
+		<>
+			<div className='hello-container fade-in'>
+				<img src='alex-reyne-logo-gradient-sm.svg' alt='alex-reyne-logo' />
+				<h1 className='hello-im'>Hello, I'm</h1>
+				<h1 className='alex-reyne'>Alexander Reyne</h1>
+			</div>
+			<div className='title'>Full-Stack Web Developer</div>
+		</>
+	);
+}
