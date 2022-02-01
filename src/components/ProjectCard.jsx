c88ca8e4c55fe1e41ba9e4a0c35cb14e19a73679
Alex-Reyne <creativereyne@gmail.com>
2022-02-01 04:08:01 -0600

    create project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,19 @@
 import './ProjectCard.scss';
 
 export default function ProjectCard(props) {
-	const { image, title } = props;
+	const { image, title, description, setDialogContent, setOpen } = props;
+
+	const handleClickOpen = () => {
+		setDialogContent({ title, image, description });
+		setOpen(true);
+	};
 
 	return (
-		<div className='project-card-container fade-in'>
-			<img src={image} />
-			<h1>{title}</h1>
-		</div>
+		<>
+			<div onClick={handleClickOpen} className='project-card-container fade-in'>
+				<img src={image} />
+				<h1>{title}</h1>
+			</div>
+		</>
 	);
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import ProjectCard from './ProjectCard';
+import ProjectDialog from './ProjectDialog';
 
 export default function Projects() {
-	const junior = 'Junior';
-	const alexReyne = 'Alex Reyne';
-	const scheduler = 'Scheduler';
-	const tweeter = 'Tweeter';
+	const [open, setOpen] = useState(false);
+	const [dialogContent, setDialogContent] = useState({
+		title: '',
+		image: '',
+		description: '',
+	});
 
+	const junior = 'Junior';
+	const juniorDescription = `Junior is a full-stack web application that allows new developers to easily create a portfolio and apply for jobs or freelance work that are relevant to them. Junior also allows employers to post jobs and gig work for developers to pick up. Junior uses a REST api backend with Express.js, PostgresSQL, and Twilio. The frontend was built with React, React Router, SASS, and MaterialUI (MUI).`;
+	const alexReyne = 'Alex Reyne';
+	// const scheduler = 'Scheduler';
+	// const tweeter = 'Tweeter';
+
+	// const schedulerImage =
+	// 	'https://github.com/Alex-Reyne/scheduler/blob/master/public/alex-reyne-scheduler.png?raw=true';
+	// const tweeterImage =
+	// 	'https://raw.githubusercontent.com/Alex-Reyne/tweeter/master/docs/Screen%20Shot%202021-11-18%20at%208.42.44%20PM.png';
 	const juniorImage =
 		'https://github.com/Alex-Reyne/junior/blob/main/frontend/public/videos/junior-dev-page.gif?raw=true';
 
@@ -14,14 +28,33 @@ export default function Projects() {
 			<h1>Recent Projects</h1>
 			<section className='projects'>
 				<div>
-					<ProjectCard image={juniorImage} title={junior} />
-					<ProjectCard image={juniorImage} title={junior} />
+					<ProjectCard
+						image={juniorImage}
+						title={junior}
+						description={juniorDescription}
+						open={open}
+						setOpen={setOpen}
+						setDialogContent={setDialogContent}
+					/>
+					<ProjectCard
+						image={juniorImage}
+						title={alexReyne}
+						// description={alexReyneDescription}
+						open={open}
+						setOpen={setOpen}
+						setDialogContent={setDialogContent}
+					/>
 				</div>
-				<div id='bottom-projects'>
-					<ProjectCard image={juniorImage} title={junior} />
-					<ProjectCard image={juniorImage} title={junior} />
-				</div>
+				{/* <div id='bottom-projects'>
+					<ProjectCard image={schedulerImage} title={scheduler} />
+					<ProjectCard image={tweeterImage} title={tweeter} />
+				</div> */}
 			</section>
+			<ProjectDialog
+				dialogContent={dialogContent}
+				open={open}
+				setOpen={setOpen}
+			/>
 		</div>
 	);
 }
