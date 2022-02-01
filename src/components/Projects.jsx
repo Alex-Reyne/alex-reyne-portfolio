b0814e43ad45ee52419f15c0485d34dd4dda90ee
Alex-Reyne <creativereyne@gmail.com>
2022-02-01 04:26:45 -0600

    add new gifs for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,16 +13,14 @@ export default function Projects() {
 	const junior = 'Junior';
 	const juniorDescription = `Junior is a full-stack web application that allows new developers to easily create a portfolio and apply for jobs or freelance work that are relevant to them. Junior also allows employers to post jobs and gig work for developers to pick up. Junior uses a REST api backend with Express.js, PostgresSQL, and Twilio. The frontend was built with React, React Router, SASS, and MaterialUI (MUI).`;
 	const alexReyne = 'Alex Reyne';
-	const alexReyneDescription = `Alex Reyne is my person portfolio site built in under 8 hours using React, and SASS and MUI for the Modal. It allows potential employers, recruiters, and freelance clients to get a quick overview of who I am and what I do.`;
+	const alexReyneDescription = `Alex Reyne is my person portfolio site. built in under 8 hours using React, and SASS and MUI for the modal you're looking at right now. It allows potential employers, recruiters, and freelance clients to get a quick overview of who I am and what I do.`;
 	// const scheduler = 'Scheduler';
 	// const tweeter = 'Tweeter';
 
 	// const schedulerImage =
 	// 	'https://github.com/Alex-Reyne/scheduler/blob/master/public/alex-reyne-scheduler.png?raw=true';
-	// const tweeterImage =
-	// 	'https://raw.githubusercontent.com/Alex-Reyne/tweeter/master/docs/Screen%20Shot%202021-11-18%20at%208.42.44%20PM.png';
-	const juniorImage =
-		'https://github.com/Alex-Reyne/junior/blob/main/frontend/public/videos/junior-dev-page.gif?raw=true';
+	const alexReyneImage = 'alex-reyne-portfolio.gif';
+	const juniorImage = 'junior-recording.gif';
 
 	return (
 		<div id='projects' className='projects-container fade-in'>
@@ -38,7 +36,7 @@ export default function Projects() {
 						setDialogContent={setDialogContent}
 					/>
 					<ProjectCard
-						image={juniorImage}
+						image={alexReyneImage}
 						title={alexReyne}
 						description={alexReyneDescription}
 						open={open}
