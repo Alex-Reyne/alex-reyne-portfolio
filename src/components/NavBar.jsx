a17102c8ea83e89150a25124dc4127e12234922c
Alex-Reyne <creativereyne@gmail.com>
2022-01-31 22:37:51 -0600

    create navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+export default function NavBar() {
+	return (
+		<nav id='nav-bar'>
+			<section className='nav-links'>
+				<p>
+					<sup>01</sup>hello
+				</p>
+				<p>
+					<sup>02</sup>about
+				</p>
+				<p>
+					<sup>03</sup>projects
+				</p>
+				<p>
+					<sup>04</sup>stack
+				</p>
+			</section>
+		</nav>
+	);
+}
