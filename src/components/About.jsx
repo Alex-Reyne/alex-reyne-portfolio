90a0ce97f54cbad274de2d9ed6318667faf3bfee
Alex-Reyne <creativereyne@gmail.com>
2022-01-31 23:20:26 -0600

    add about info. add scrolltotop on navbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,32 @@
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-scroll';
+
 export default function About() {
 	return (
 		<div className='about-container fade-in'>
 			<section className='about-me'>
+				<Link to='about' spy={true} smooth={true}>
+					<FontAwesomeIcon id='about' icon={faAngleDoubleDown} />
+				</Link>
 				<h1>About Me</h1>
+				<p>
+					I’m a Full-Stack Web Developer and self taught Graphic Designer. I’ve
+					always had a passion for creating art and tinkering with things.
+				</p>
+				<p>
+					I can work independently but I also enjoy collaborating as part of a
+					team. I love to solve problems and have a high attention to detail.
+				</p>
+				<p>
+					After 10 years as a designer I realized that my passion for creative
+					works had a hole in it. With a burning desire to implement my designs
+					as functional works, I decided to dive into web development.
+				</p>
+				<p>
+					I am now ready and eager to work in and collaborate with a team of
+					likeminded developers and designers
+				</p>
 			</section>
 		</div>
 	);
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,17 @@
+import { Link, animateScroll as scroll } from 'react-scroll';
+
 export default function NavBar() {
 	return (
 		<nav id='nav-bar'>
 			<section className='nav-links'>
-				<p>
+				<p onClick={scroll.scrollToTop}>
 					<sup>01</sup>hello
 				</p>
-				<p>
-					<sup>02</sup>about
-				</p>
+				<Link to='about' spy={true} smooth={true}>
+					<p>
+						<sup>02</sup>about
+					</p>
+				</Link>
 				<p>
 					<sup>03</sup>projects
 				</p>
