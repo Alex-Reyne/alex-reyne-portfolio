4d749a6b19c9fde78746a3cff29f9299123fca02
Alex-Reyne <creativereyne@gmail.com>
2022-02-02 20:01:32 -0600

    add navbar hover states

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,54 @@
 import '../styles/NavBar.scss';
+import useSound from 'use-sound';
 import { Link, animateScroll as scroll } from 'react-scroll';
 
 export default function NavBar() {
+	const [play] = useSound('../images/click.mp3');
+
 	return (
 		<nav id='nav-bar'>
 			<section className='nav-links'>
-				<p onClick={scroll.scrollToTop}>
-					<sup>01</sup>hello
-				</p>
+				<Link>
+					<sup>01</sup>
+					<p
+						className='.btn-flip'
+						class='btn-flip'
+						data-back='Hello'
+						data-front='Hello'
+						onClick={scroll.scrollToTop}
+						onMouseEnter={play}
+					></p>
+				</Link>
+
 				<Link to='about' spy={true} smooth={true}>
-					<p onClick={e => teste.play()}>
-						<sup>02</sup>about
-					</p>
+					<sup>02</sup>
+					<p
+						className='.btn-flip'
+						class='btn-flip'
+						data-back='About'
+						data-front='About'
+						onMouseEnter={play}
+					></p>
 				</Link>
 				<Link to='projects' spy={true} smooth={true}>
-					<p>
-						<sup>03</sup>projects
-					</p>
+					<sup>03</sup>
+					<p
+						className='.btn-flip'
+						class='btn-flip'
+						data-back='Projects'
+						data-front='Projects'
+						onMouseEnter={play}
+					></p>
 				</Link>
 				<Link to='stack' spy={true} smooth={true}>
-					<p>
-						<sup>04</sup>stack
-					</p>
+					<sup>04</sup>
+					<p
+						className='.btn-flip'
+						class='btn-flip'
+						data-back='Stack'
+						data-front='Stack'
+						onMouseEnter={play}
+					></p>
 				</Link>
 			</section>
 		</nav>
