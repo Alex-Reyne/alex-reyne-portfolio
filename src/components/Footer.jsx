3c094e8d6af3f0998e74286f527537afb55abe22
Alex-Reyne <creativereyne@gmail.com>
2022-02-07 11:55:11 -0600

    add media query for phones on website footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
 	return (
 		<footer>
 			<Stack />
-			<p>&copy; Alex Reyne 2022</p>
+			<p>Designed and Developed by &copy; Alex Reyne 2022</p>
 		</footer>
 	);
 }
